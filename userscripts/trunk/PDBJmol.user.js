e1369874fbac1c1618872e8dc9c8ed785540dff2
baoilleach <baoilleach@d3c5a50b-8e31-0410-8099-a759d7a8ce2e>
2007-10-22 13:06:28 +0000

    Noel: Adding an automatic update facility

--- userscripts/trunk/PDBJmol.user.js
+++ userscripts/trunk/PDBJmol.user.js
@@ -158,4 +158,37 @@ function run() {
 // MAIN
 run();
 
+var d = new Date();
+var curr_date = d.getDate();
+var date_last_checked= GM_getValue("check_updates", 0);
+if (date_last_checked != curr_date)
+{
+  GM_setValue("check_updates", curr_data);
+  // Modified the code by Seifer at http://userscripts.org/users/33118
+  script_name = 'PBDJmol';
+  script_num = '8033';
+  script_href = "http://blueobelisk.svn.sf.net/svnroot/blueobelisk/userscripts/trunk/PDBJmol.user.js";
+  script_as_text = "http://blueobelisk.svn.sourceforge.net/viewvc/*checkout*/blueobelisk/userscripts/trunk/PDBJmol.user.js?content-type=text%2Fplain";
+  script_version=0.9;
+  script_updatetext='Added an updater';
+
+  GM_xmlhttpRequest({
+      method: "GET",
+      // url: "http://userscripts.org/scripts/review/"+script_num+"?format=txt",
+      url: script_as_text;
+      onload: function(responseDetails) {
+        var text = responseDetails.responseText;
+        var update_version = text.substring(text.indexOf("script_version=")+15,text.indexOf("\n",text.indexOf("script_version="))-2);
+        var update_text = text.substring(text.indexOf("script_updatetext=")+19,text.indexOf("\n",text.indexOf("script_updatetext="))-3);
+      if(update_version > script_version) {
+          newversion = document.createElement("div");
+          newversion.setAttribute("id", "gm_update_alert");
+          newversion.setAttribute("style", "background-color:yellow; width:100%; position:absolute; z-index:99; top:0px; left:0px; text-align:center; font-size:11px; font-family: Tahoma");
+          newversion.innerHTML = "<a href='#' onclick='document.body.removeChild(document.getElementById(&quot;gm_update_alert&quot;))' style='color:red'>Close</a><font color='yellow'>--------</font><font color='red'>There is a new version of the &quot;"+script_name+"&quot; script. You are currently running version "+script_version+".</font><br><font color='yellow'>----------------</font>The latest version is "+update_version+". <a href='#' onclick='document.getElementById(&quot;gm_update_alert_info&quot;).setAttribute(&quot;style&quot;, &quot;display:block&quot;)' style='color:green'>Click here for more info</a> or <a style='color:green' href='" + script_href + "'><b>Click here to download the latest version</b></a><span id='gm_update_alert_info' style='display:none'><b>Here's a short description of the latest update...</b><br>"+update_text+"</span>";
+          document.body.appendChild(newversion);
+        }
+      }
+  });
+}
+
 
