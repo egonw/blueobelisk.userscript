b58698c64056d6384e5341d9a9c29ce33725bb71
baoilleach <baoilleach@d3c5a50b-8e31-0410-8099-a759d7a8ce2e>
2007-10-23 12:15:47 +0000

    (1) Added code to automatically update. (2) Replaced my popup with Connotea's popup

--- userscripts/trunk/AddtoConnotea.user.js
+++ userscripts/trunk/AddtoConnotea.user.js
@@ -15,6 +15,7 @@
 // @include       http://www.malariajournal.com/*
 // @include       http://*.oxfordjournals.org/*
 // @include       http://*.plosjournals.org/*
+// @include       http://www.biomedcentral.com/*
 // @include       http://www.pnas.org/*
 // ==/UserScript==
 //
@@ -42,6 +43,14 @@ function enterusername(ev) {
   button.addEventListener("click", changeusername, true);
   var cancel = document.getElementById("stackitusernamecancel");
   cancel.addEventListener("click", cancelusername, true);
+}
+
+function submit_simple(ev) {
+  var doi = "http://dx.doi.org/" + ev.target.id;
+  // x=window; 
+  w=open('http://www.connotea.org/addpopup?continue=confirm&uri='+escapeURL(doi), 
+      'add', 'width=600, height=400, scrollbars, resizable'); 
+  // void(x.setTimeout('w.focus()',200));	
 }
 
 function submit(ev) {
@@ -81,19 +90,24 @@ function submit(ev) {
   cancel.addEventListener("click", cancelsubmit, true);
 }
 
-function insertaddtoconnotea(doi, textarea) {
+function insertaddtoconnotea(doi, textarea, type) {
+  // type is "simple" or "advanced"
   space = document.createElement("span");
   space.innerHTML = "&nbsp;";
   numstacks = document.createElement("span");
   numstacks.setAttribute("style", "font-weight:bold; font-size: 14; font-family: Arial;");
-  // numstacks.setAttribute("href", "http://www.connotea.org/uri/" + doi);
   img = document.createElement("img");
   img.setAttribute("alt","Connotea");
   img.setAttribute("src","http://www.connotea.org/connotea_icon.png");
   img.setAttribute("style", "position:relative; bottom: -4px");
   img.setAttribute("border","0");
   img.setAttribute("id",doi); // This is 'passed' to the submit callback
-  img.addEventListener("click", submit, true);
+  if (type=="advanced") {
+    img.addEventListener("click", submit, true);
+  }
+  else {
+    img.addEventListener("click", submit_simple, true);
+  }
 
   //insert the created nodes before the next sibling of the DOI containing node
   textarea.parentNode.insertBefore(numstacks, textarea.nextSibling);
@@ -333,8 +347,40 @@ function testnoop() {
 }
 
 function main() {
-  testnoop();
+  // Check for updates once a day
+  var d = new Date();
+  var curr_date = d.getDate();
+  var date_last_checked= GM_getValue("check_updates", 0);
+  if (date_last_checked != curr_date)
+  {
+    GM_setValue("check_updates", curr_date);
+    // Modified the code by Seifer at http://userscripts.org/users/33118
+    script_name = 'AddtoConnotea.user.js';
+    script_href = "http://blueobelisk.svn.sf.net/svnroot/blueobelisk/userscripts/trunk/" + script_name;
+    script_as_text = "http://blueobelisk.svn.sourceforge.net/viewvc/*checkout*/blueobelisk/userscripts/trunk/" + script_name + "?content-type=text%2Fplain";
+    script_version=1.0;
+    script_updatetext='ADD UPDATE TEXT HERE';
+
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: script_as_text,
+        onload: function(responseDetails) {
+          var text = responseDetails.responseText;
+          var update_version = text.substring(text.indexOf("script_version=")+15,text.indexOf("\n",text.indexOf("script_version="))-2);
+          var update_text = text.substring(text.indexOf("script_updatetext=")+19,text.indexOf("\n",text.indexOf("script_updatetext="))-3);
+        if(update_version > script_version) {
+            newversion = document.createElement("div");
+            newversion.setAttribute("id", "gm_update_alert");
+            newversion.setAttribute("style", "background-color:yellow; width:100%; position:absolute; z-index:99; top:0px; left:0px; text-align:center; font-size:11px; font-family: Tahoma");
+            newversion.innerHTML = "<a href='#' onclick='document.body.removeChild(document.getElementById(&quot;gm_update_alert&quot;))' style='color:red'>Close</a><font color='yellow'>--------</font><font color='red'>There is a new version of the &quot;"+script_name+"&quot; script. You are currently running version "+script_version+".</font><br><font color='yellow'>----------------</font>The latest version is "+update_version+". <a href='#' onclick='document.getElementById(&quot;gm_update_alert_info&quot;).setAttribute(&quot;style&quot;, &quot;display:block&quot;)' style='color:green'>Click here for more info</a> or <a style='color:green' href='" + script_href + "'><b>Click here to download the latest version</b></a><span id='gm_update_alert_info' style='display:none'><b>Here's a short description of the latest update...</b><br>"+update_text+"</span>";
+            document.body.appendChild(newversion);
+          }
+        }
+    });
   }
+  // Start the rest of the program...
+  testnoop();
+}
 
 
 function bigmain() {
@@ -361,7 +407,7 @@ function bigmain() {
         var doi_found=RegExp.$1;
         if (ar && doi_found){
           //getnumstacks(doi_found, thisTextarea);
-          numstacks = insertaddtoconnotea(doi_found, thisTextarea);
+          numstacks = insertaddtoconnotea(doi_found, thisTextarea, "simple");
           queue.push( [doi_found, numstacks, 0] );
         }
       }
@@ -369,3 +415,4 @@ function bigmain() {
   lookupDOI();
   }
 }
+
